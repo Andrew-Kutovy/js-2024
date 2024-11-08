5947e21ee3632cc1f0b1f57be6ec0b18a7084713
AndrewKutovy928 <akutovy928@gmail.com>
2024-11-08 13:42:01 +0200

    hw 3 was corrected; +/- 4 hw not complyte

--- homeworks/hw3/main.js
+++ homeworks/hw3/main.js
@@ -7,15 +7,15 @@ for (let i =0; i<10; i++) document.write(`<div>ffffhfh ${i}</div>`);
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 let x = 0;
 while (x<20) {
-    document.write('<div>while</div>');
-    x++;
+     document.write('<div>while</div>');
+     x++;
 }
 
 // - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
 let x1 = 0;
 while (x1<20) {
-    document.write(`<div>while ${x1}</div>`);
-    x1++;
+     document.write(`<div>while ${x1}</div>`);
+     x1++;
 }
 
 // - Використовуючи данні з масиву, за допомоги document.write та циклу
@@ -35,7 +35,7 @@ let listOfItems = ['html', 'css', 'javascript', 'mysql', 'mongodb', 'react', 'an
 // замість 'ITEM OF ARRAY' підставити елемент з масиву щоб получився цілий список з даними з масиву
 // -----------------------------------------------
 document.write('<ul>');
-for (let i = 0; i < listOfItems.length; i++) document.write(`<li>${listOfItems[i]}</li>`);
+for (let item of listOfItems) document.write(`<li>${item}</li>`);
 document.write('</ul>');
 
 //     Використовуючи данні з масиву, за допомоги document.write та циклу
@@ -43,26 +43,26 @@ document.write('</ul>');
 // Великими літерами прописанні властивості об'єкту які потрібно впровадити в шаблон
 //
 let products = [
-    {
-        title: 'milk',
-        price: 22,
-        image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
-    },
-    {
-        title: 'juice',
-        price: 27,
-        image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
-    },
-    {
-        title: 'tomato',
-        price: 47,
-        image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
-    },
-    {
-        title: 'tea',
-        price: 15,
-        image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
-    },
+     {
+          title: 'milk',
+          price: 22,
+          image: 'https://www.mcqueensdairies.co.uk/wp-content/uploads/2019/02/Mcqueens_1litre_whole_organic-300x300-3.jpg'
+     },
+     {
+          title: 'juice',
+          price: 27,
+          image: 'https://images-na.ssl-images-amazon.com/images/I/61jL2GCuKLL._SX679_PIbundle-24,TopRight,0,0_AA679SH20_.jpg'
+     },
+     {
+          title: 'tomato',
+          price: 47,
+          image: 'https://dictionary.cambridge.org/ru/images/thumb/tomato_noun_001_17860.jpg?version=5.0.74'
+     },
+     {
+          title: 'tea',
+          price: 15,
+          image: 'https://yogiproducts.com/wp-content/uploads/2009/03/YT-US-CAR-RelaxedMind-C23-202201-V2-3DFront_withGlow-300DPI-1.png'
+     },
 ];
 // ШАБЛОН
 // <div class="product-card">
@@ -71,30 +71,44 @@ let products = [
 // </div>
 // Замість TITLE PRICE IMAGE - підставити відповідні поля з об'єкту
 // --------------------
-for (let i = 0; i < products.length; i++) {
-    document.write(`<div class="product-card">
-        <h3 class="product-title">${products[i]["title"]}. Price - ${products[i]["price"]}</h3>
-        <img src="${products[i]["image"]}" alt="${products[i]["title"]}" class="product-image"></div>`)
+for (let product of products) {
+     document.write(`<div class="product-card">
+        <h3 class="product-title">${product["title"]}. Price - ${product["price"]}</h3>
+        <img src="${product["image"]}" alt="${product["title"]}" class="product-image"></div>`)
 }
 
 // є масив
 let users = [
-    {name: 'vasya', age: 31, status: false},
-    {name: 'petya', age: 30, status: true},
-    {name: 'kolya', age: 29, status: true},
-    {name: 'olya', age: 28, status: false},
-    {name: 'max', age: 30, status: true},
-    {name: 'anya', age: 31, status: false},
-    {name: 'oleg', age: 28, status: false},
-    {name: 'andrey', age: 29, status: true},
-    {name: 'masha', age: 30, status: true},
-    {name: 'olya', age: 31, status: false},
-    {name: 'max', age: 31, status: true}
+     {name: 'vasya', age: 31, status: false},
+     {name: 'petya', age: 30, status: true},
+     {name: 'kolya', age: 29, status: true},
+     {name: 'olya', age: 28, status: false},
+     {name: 'max', age: 30, status: true},
+     {name: 'anya', age: 31, status: false},
+     {name: 'oleg', age: 28, status: false},
+     {name: 'andrey', age: 29, status: true},
+     {name: 'masha', age: 30, status: true},
+     {name: 'olya', age: 31, status: false},
+     {name: 'max', age: 31, status: true}
 ];
 // за допомоги циклу вивести:
-// - користувачів зі статусом true
-for (let i = 0; i< users.length; i++) if (users[i]["status"] === true) console.log(users[i]);
-// - користувачів зі статусом false
-for (let i = 0; i< users.length; i++) if (users[i]["status"] === false) console.log(users[i]);
-// - користувачів які старші за 30 років
-for (let i = 0; i<users.length; i++) if (users[i]["age"] > 30) console.log(users[i]);
+console.log('користувачі зі статусом true')
+for (let user of users) {
+     if (user["status"] === true) {
+          console.log(user);
+     }
+}
+
+console.log('користувачі зі статусом false')
+for (let user of users) {
+     if (user["status"] === false) {
+          console.log(user);
+     }
+}
+
+console.log('користувачів які старші за 30 років')
+for (let user of users) {
+     if (user["age"] > 30) {
+          console.log(user);
+     }
+}
